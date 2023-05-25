1ae854ebfeeba54130af21e7876989a923e3fa44
Nasrin Nazarihashemi <nasrin@Nasrins-MBP.lan>
2023-05-25 00:38:47 -0400

    creating user documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,13 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  Firestore
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwQGVGxsen6KPyR6ybufCceBFigN7HA1E",
@@ -24,3 +31,27 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  
+  const userSnapshot = await getDoc(userDocRef);
+  
+
+  if(!userSnapshot.exists()){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error){
+      console.log('error creating the user', error.message);
+    }
+  }
+  return userDocRef;
+};
